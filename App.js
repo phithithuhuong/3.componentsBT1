b15733917d1f03e0bee65a70eadab0a29094981c
phithithuhuong <thu872315@gmail.com>
2023-03-14 10:50:42 +0700

    bt1 alert

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Alert from "./Alert";
+
+function App() {
+  return (
+    <div className="App">
+<Alert text="Cảnh báo! Bạn đã vi phạm bản quyền của tôi, vui lòng nộp phạt."/>
+    </div>
+  );
+}
+
+export default App;
